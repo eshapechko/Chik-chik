bc2fc40a5c49f58d0d4b020b8cd787f16974fccc
Evgeniy Shapechko <shapen86@mail.ru>
2023-02-09 15:54:37 +0300

    add api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const API_URL = "https://daisy-pepper-denim.glitch.me/";
+
+const year = new Date().getFullYear();
+
+/*
+GET /api - получить список услуг
+GET /api?service={n} - получить список барберов
+GET /api?spec={n} - получить список месяца работы барбера
+GET /api?spec={n}&month={n} - получить список дней работы барбера
+GET /api?spec={n}&month={n}&day={n} - получить список свободных часов барбера
+POST /api/order - оформить заказ
+*/
+
 const addPreload = (elem) => {
     elem.classList.add("preload");
 };
@@ -85,4 +98,257 @@ const initSlider = () => {
         startSlider();
     });
 };
-window.addEventListener("DOMContentLoaded", initSlider);
+
+const renderPrice = (wrapper, data) => {
+    data.forEach((item) => {
+        const priceItem = document.createElement("li");
+        priceItem.classList.add("price__item");
+
+        priceItem.innerHTML = `
+        <span class="price__item-title">${item.name}</span>
+        <span class="price__item-count">${item.price} руб</span>
+        `;
+        // console.log(priceItem);
+
+        wrapper.append(priceItem);
+    });
+    /*
+    <li class="price__item">
+        <span class="price__item-title">Оформление бровей</span>
+        <span class="price__item-count">1000 руб</span>
+    </li>
+    */
+};
+
+const renderService = (wrapper, data) => {
+    const labels = data.map((item) => {
+        const label = document.createElement("label");
+        label.classList.add("radio");
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="service" value='${item.id}'/>
+            <span class="radio__label">${item.name}</span>
+        `;
+        return label;
+    });
+
+    // console.log(labels);
+    // console.log(...labels);
+    wrapper.append(...labels);
+
+    /*
+        <label class="radio">
+            <input class="radio__input" type="radio" name="service" />
+            <span class="radio__label">Стрижка ножницами</span>
+        </label>
+    */
+};
+
+const initService = () => {
+    const priceList = document.querySelector(".price__list");
+    const reserveFieldsetService = document.querySelector(".reserve__fieldset_service");
+    priceList.textContent = "";
+    addPreload(priceList);
+
+    reserveFieldsetService.innerHTML = ` <legend class="reserve__legend">Услуга</legend>`;
+    addPreload(reserveFieldsetService);
+    fetch(`${API_URL}/api`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            renderPrice(priceList, data);
+            removePreload(priceList);
+            return data;
+        })
+        .then((data) => {
+            renderService(reserveFieldsetService, data);
+            removePreload(reserveFieldsetService);
+        });
+};
+
+const addDisabled = (arr) => {
+    arr.forEach((elem) => {
+        elem.disabled = true;
+    });
+};
+
+const removeDisabled = (arr) => {
+    arr.forEach((elem) => {
+        elem.disabled = false;
+    });
+};
+
+const renderSpec = (wrapper, data) => {
+    const labels = data.map((item) => {
+        const label = document.createElement("label");
+        label.classList.add("radio");
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="spec" value="${item.id}"/>
+            <span class="radio__label radio__label_spec"
+                style="--bg-image: url(${API_URL}${item.img})">${item.name}</span>
+        `;
+        return label;
+    });
+
+    wrapper.append(...labels);
+};
+
+const renderMonth = (wrapper, data) => {
+    const labels = data.map((month) => {
+        const label = document.createElement("label");
+        label.classList.add("radio");
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="month" value="${month}"/>
+            <span class="radio__label">${new Intl.DateTimeFormat("ru-RU", {
+                month: "long",
+            }).format(new Date(year, month))}</span>
+        `;
+        return label;
+    });
+
+    wrapper.append(...labels);
+};
+
+const renderDay = (wrapper, data, month) => {
+    const labels = data.map((day) => {
+        const label = document.createElement("label");
+        label.classList.add("radio");
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="day" value="${day}"/>
+            <span class="radio__label">${new Intl.DateTimeFormat("ru-RU", {
+                month: "long",
+                day: "numeric",
+            }).format(new Date(year, month, day))}</span>
+        `;
+        return label;
+    });
+
+    wrapper.append(...labels);
+};
+
+const renderTime = (wrapper, data) => {
+    const labels = data.map((time) => {
+        const label = document.createElement("label");
+        label.classList.add("radio");
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="time" value="${time}"/>
+            <span class="radio__label">${time}</span>
+        `;
+        return label;
+    });
+
+    wrapper.append(...labels);
+};
+
+const initReserve = () => {
+    const reserveForm = document.querySelector(".reserve__form");
+    // console.log("reserveForm: ", reserveForm);
+    const { fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = reserveForm;
+
+    addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+
+    reserveForm.addEventListener("change", async (event) => {
+        const target = event.target;
+        // console.log("target: ", target);
+
+        if (target.name === "service") {
+            addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+            fieldspec.innerHTML = ' <legend class="reserve__legend">Специалист</legend>';
+            addPreload(fieldspec);
+
+            const response = await fetch(`${API_URL}/api?service=${target.value}`);
+            // console.log("response: ", response);
+            const data = await response.json();
+            // console.log("data: ", data);
+            renderSpec(fieldspec, data);
+            removePreload(fieldspec);
+            removeDisabled([fieldspec]);
+        }
+
+        if (target.name === "spec") {
+            addDisabled([fielddata, fieldmonth, fieldday, fieldtime, btn]);
+            addPreload(fieldmonth);
+
+            const response = await fetch(`${API_URL}/api?spec=${target.value}`);
+            const data = await response.json();
+
+            fieldmonth.textContent = "";
+
+            renderMonth(fieldmonth, data);
+            removePreload(fieldmonth);
+            removeDisabled([fielddata, fieldmonth]);
+        }
+
+        if (target.name === "month") {
+            addDisabled([fieldday, fieldtime, btn]);
+            addPreload(fieldday);
+
+            const response = await fetch(
+                `${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}`
+            );
+            const data = await response.json();
+
+            fieldday.textContent = "";
+
+            renderDay(fieldday, data, reserveForm.month.value);
+            removePreload(fieldday);
+            removeDisabled([fieldday]);
+        }
+
+        if (target.name === "day") {
+            addDisabled([fieldtime, btn]);
+            addPreload(fieldtime);
+
+            const response = await fetch(
+                `${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}&day=${target.value}`
+            );
+            const data = await response.json();
+
+            fieldtime.textContent = "";
+
+            renderTime(fieldtime, data);
+            removePreload(fieldtime);
+            removeDisabled([fieldtime]);
+        }
+
+        if (target.name === "time") {
+            removeDisabled([btn]);
+        }
+    });
+
+    reserveForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(reserveForm);
+        const json = JSON.stringify(Object.fromEntries(formData));
+
+        const response = await fetch(`${API_URL}api/order`, {
+            method: "post",
+            body: json,
+        });
+
+        const data = await response.json();
+
+        addDisabled([fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+
+        const p = document.createElement("p");
+        p.textContent = `
+        Спасибо за бронь #${data.id}!
+        Ждём вас ${new Intl.DateTimeFormat("ru-RU", {
+            month: "long",
+            day: "numeric",
+        }).format(new Date(year, data.month, data.day))},
+        время ${data.time}
+        `;
+
+        reserveForm.append(p);
+    });
+};
+
+const init = () => {
+    initSlider();
+    initService();
+    initReserve();
+};
+window.addEventListener("DOMContentLoaded", init);
